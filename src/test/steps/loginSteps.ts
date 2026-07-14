b0d4a7dee6a5c6990e5ee6dde8eebd0c8988ee56
Sanmitha4 <sanmithashetty2004@gmail.com>
2026-07-14 06:05:50 +0000

    Login:Login succeeds with standard valid credentials

--- src/test/steps/loginSteps.ts
+++
@@ -1,7 +0,0 @@
-Feature: User Login Authentication
-
-  @Key:Login_01 @regression @sauce @positive
-  Scenario: Login succeeds with standard valid credentials
-    Given User navigates to the Sauce Demo login page
-    When User attempts to login with credentials from test data "standard_user"
-    Then User should be successfully redirected to the dashboard page
